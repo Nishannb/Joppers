fb0af9be381c48cb7f5122afe96a8a464e9e63ec
TheVoidThatConsumes <lyricalluxor@gmail.com>
2022-11-16 10:25:21 +0100

    Final Commit #1
    Built on my local machine and committed for the first time to Github.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      spacing: {
+        stressed: '92px',
+      },
+    },
+  },
+  plugins: [],
+}
